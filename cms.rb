6df697bc44ef73cd1fffb910decdae3ab5f184d5
riazmohamed <railsdevriaz@gmail.com>
2022-04-18 10:28:03 +0100

    Launch school solution

--- cms.rb
+++ cms.rb
@@ -3,10 +3,11 @@ require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
 
-get '/' do
-  "Getting started."
-  files = Dir.glob("data/*.*")
-  @file_names = files.map { |file| File.basename(file) }.sort
+root = File.expand_path("..", __FILE__)
 
+get "/" do
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
   erb :index
 end
