b89f9cde3742d02a7a17aa290282458b4654c89e
riazmohamed <railsdevriaz@gmail.com>
2022-04-18 18:45:07 +0100

    Handling requests for Nonexistent Documents

--- cms.rb
+++ cms.rb
@@ -3,6 +3,11 @@ require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
 root = File.expand_path("..", __FILE__)
 
 get "/" do
@@ -15,6 +20,11 @@ end
 get "/:filename" do
   file_path = root + "/data/" + params[:filename]
 
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+  if File.file?(file_path)
+    headers["Content-Type"] = "text/plain"
+    File.read(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
 end
