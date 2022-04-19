47062095f4b4fbfb3b9354bd925dcd56fd2aad7a
riazmohamed <railsdevriaz@gmail.com>
2022-04-19 10:52:34 +0100

    Isolate data for testing environment

--- cms.rb
+++ cms.rb
@@ -27,8 +27,18 @@ def load_file_content(path)
   end
 end
 
+# Correct path for the documents to be stored in the current environment
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
     File.basename(path)
   end
   erb :index
@@ -36,7 +46,7 @@ end
 
 # View a single file in the list
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   if File.exist?(file_path)
     load_file_content(file_path)
@@ -48,7 +58,7 @@ end
 
 # Edit document content
 get "/:filename/edit" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   @filename = params[:filename]
   @content = File.read(file_path)
@@ -58,7 +68,7 @@ end
 
 # Update document content
 post "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   File.write(file_path, params[:content])
 
