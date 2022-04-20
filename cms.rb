ccd24103351a9186f203b6658996cd5336e6ac05
riazmohamed <railsdevriaz@gmail.com>
2022-04-20 08:58:22 +0100

    User signin and signout

--- cms.rb
+++ cms.rb
@@ -107,3 +107,26 @@ post "/:filename/delete" do
   session[:message] = "#{params[:filename]} has been deleted."
   redirect "/"
 end
+
+# User sign in and sign out
+get "/users/signin" do
+  erb :signin
+end
+
+post "/users/signin" do
+  if params[:username] == "admin" && params[:password] == "secret"
+    session[:username] = params[:username]
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid credentials"
+    status 422
+    erb :signin
+  end
+end
+
+post "/users/signout" do
+  session.delete(:username)
+  session[:message] = "You have been signed out."
+  redirect "/"
+end
